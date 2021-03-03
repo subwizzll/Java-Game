529374300192f3da220d6dd584177cbf066394a6
subwizzll <71910743+subwizzll@users.noreply.github.com>
2021-03-03 17:44:59 -0500

    no change

--- src/com/game/Main.java
+++ src/com/game/Main.java
@@ -3,5 +3,5 @@ package com.game;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello");    }
+        System.out.println("hello im sophie");    }
 }
