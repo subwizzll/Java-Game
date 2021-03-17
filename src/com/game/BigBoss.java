8938f70ff6ae99666027eb902b2a447d8b63cf4b
Sophie <sophietanker@gmail.com>
2021-03-17 14:44:15 -0400

    Messing around with color and appearance changes. Changed head of projectiles to blue.

--- src/com/game/BigBoss.java
+++ src/com/game/BigBoss.java
@@ -1,32 +1,42 @@
 package com.game;
 
 import java.awt.*;
+import java.util.Random;
+
 //This is a work in progress for the end of game boss.
 public class BigBoss extends GameObject{
 
+    private int timer = 0;
+    private Random r;
+
     public BigBoss(float x, float y, GameObjectID id, Handler handler) {
         super(x, y, id,handler);
-        width = 20;
-        height = 20;
-        velX = 5;
-        velY = 5;
+        width = 64;
+        height = 64;
+        velX = 1;
+        velY = 1;
     }
 
     @Override
     public void tick() {                    // method to synchronize object with game loop
-        handler.addObject(new Trail(x,y,GameObjectID.Trail,handler,0.05f)); // trailing effect
-        x += velX;
-        y += velY;
-
-        if(x <= 0 || x >= Game.WIDTH - 16) velX *= -1;  // prevents object from leaving the window
-        if(y <= 0 || y >= Game.HEIGHT - 32) velY *= -1; //
-
+        if(y < 50 && timer == 0) {          // timer starts when boss reaches panning home
+            y += velY;
+        }
+        else {
+            timer++;
+            x += velX;
+        }
+        if (timer > 0 && timer < 1000) {
+            if (x <= 0 || x >= Game.WIDTH - 16) velX *= -1;  // prevents object from leaving the window
+            if (timer % 50 == 0 ){                               // fires smart enemies every 0.5 seconds
+                handler.addObject(new SmartEnemy((int)x+32,(int)y+32, GameObjectID.SmartEnemy, handler));
+            }
+        }
         collision();
     }
 
     @Override
     public void render(Graphics g) {        // object rendering method
-        //Testing made me realize this sets projectile head color and not boss color. -S
         Color testBlue = new Color(50, 168, 149);
         g.setColor(testBlue);
         g.fillRect((int)x,(int)y,(int)width,(int)height);
@@ -38,17 +48,5 @@ public class BigBoss extends GameObject{
     }
 
     private void collision(){                // handles collision characteristics for this object
-
-        for (int i = 0; i < handler.object.size(); i++) {
-
-            GameObject tempObject = handler.object.get(i);
-
-            if(tempObject.getId() == GameObjectID.Player){
-                if(getBounds().intersects(tempObject.getBounds())){
-                    velX *= -1;
-                    velY *= -1;
-                }
-            }
-        }
     }
 }
